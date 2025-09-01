0abf2e6f0054d1815443a8b604a0874d25df0ae7
Tomislav Lazar <tomislav@morgancode.com>
2025-09-01 21:33:26 +0200

    date

--- src/Models/LeaveAllowance.php
+++ src/Models/LeaveAllowance.php
@@ -4,10 +4,11 @@ namespace Amicus\FilamentEmployeeManagement\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class LeaveAllowance extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes ;
 
     protected $fillable = [
         'employee_id',
