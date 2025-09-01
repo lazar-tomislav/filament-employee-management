e3f70884f947d37881600a0d17d52dfa7733b37e
lazar-tomislav <83224193+lazar-tomislav@users.noreply.github.com>
2025-09-01 19:41:05 +0000

    Fix styling

--- src/Models/LeaveAllowance.php
+++ src/Models/LeaveAllowance.php
@@ -8,7 +8,8 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class LeaveAllowance extends Model
 {
-    use HasFactory, SoftDeletes ;
+    use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = [
         'employee_id',
