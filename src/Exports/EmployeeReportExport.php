2465cbd2fe7d18bac4d1b5806a8bea700d9582b9
Tomislav Lazar <tomislav@morgancode.com>
2025-09-06 13:38:04 +0200

    style

--- src/Exports/EmployeeReportExport.php
+++ src/Exports/EmployeeReportExport.php
@@ -2,14 +2,14 @@
 
 namespace Amicus\FilamentEmployeeManagement\Exports;
 
+use Amicus\FilamentEmployeeManagement\Enums\LeaveRequestType;
 use Amicus\FilamentEmployeeManagement\Models\TimeLog;
 use Amicus\FilamentEmployeeManagement\Models\LeaveRequest;
+use Amicus\FilamentEmployeeManagement\Models\Employee;
 use Maatwebsite\Excel\Concerns\FromArray;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithStyles;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
-use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
-use PhpOffice\PhpSpreadsheet\Style\Fill;
 use Carbon\Carbon;
 
 class EmployeeReportExport implements FromArray, WithHeadings, WithStyles, ShouldAutoSize
@@ -17,73 +17,108 @@ class EmployeeReportExport implements FromArray, WithHeadings, WithStyles, Shoul
     protected int $employeeId;
     protected int $month;
     protected int $year;
-    
+    protected Employee $employee;
+
+    protected int $totalWorkHours = 0;
+    protected ?int $totalVacationHours = 0;
+    protected ?int $totalSickLeaveHours = 0;
+    protected ?int $totalOvertimeHours = 0;
+    protected ?int $totalOtherHours = 0;
+
     public function __construct(int $employeeId, int $month, int $year)
     {
         $this->employeeId = $employeeId;
         $this->month = $month;
         $this->year = $year;
+        $this->employee = Employee::find($employeeId);
     }
 
     public function array(): array
     {
         $data = [];
         $daysInMonth = Carbon::create($this->year, $this->month)->daysInMonth;
-        
-        $totalWorkHours = 0;
-        $totalVacationHours = 0;
-        $totalSickLeaveHours = 0;
-        $totalOvertimeHours = 0;
-        $totalOtherHours = 0;
 
         for ($day = 1; $day <= $daysInMonth; $day++) {
             $date = Carbon::create($this->year, $this->month, $day);
-            $dayName = $this->getDayNameInCroatian($date->dayOfWeek);
-            
-            // Dohvati radne sate za taj dan
-            $dailyWorkHours = $this->getDailyWorkHours($date);
-            $totalWorkHours += $this->convertTimeToMinutes($dailyWorkHours);
-            
-            // Dohvati godišnji odmor
-            $vacationHours = $this->getLeaveHours($date, 'vacation');
-            $totalVacationHours += $this->convertTimeToMinutes($vacationHours);
-            
-            // Dohvati bolovanje
-            $sickLeaveHours = $this->getLeaveHours($date, 'sick_leave');
-            $totalSickLeaveHours += $this->convertTimeToMinutes($sickLeaveHours);
-            
-            // Prekovremeni sati - placeholder za sada
-            $overtimeHours = TimeLog::getOvertimeHoursForDate($this->employeeId, $date->format('Y-m-d'));
-            $totalOvertimeHours += $this->convertTimeToMinutes($overtimeHours);
-            
-            // Ostalo
-            $otherHours = $this->getLeaveHours($date, 'other');
-            $totalOtherHours += $this->convertTimeToMinutes($otherHours);
+
+            $this->totalWorkHours = $this->getDailyWorkHours($date);
+            $this->totalVacationHours = $this->getLeaveHours($date, LeaveRequestType::ANNUAL_LEAVE);
+            $this->totalSickLeaveHours = $this->getLeaveHours($date, LeaveRequestType::ANNUAL_LEAVE);
+            $this->totalOvertimeHours = TimeLog::getOvertimeHoursForDate($this->employeeId, $date->format('Y-m-d'));
+            $this->totalOtherHours = $this->getLeaveHours($date, LeaveRequestType::PAID_LEAVE);
 
             $data[] = [
                 $day,
-                strtoupper($dayName),
-                $dailyWorkHours ?: '',
-                $vacationHours ?: '',
-                $sickLeaveHours ?: '',
-                $overtimeHours ?: '',
-                $otherHours ?: '',
+                strtoupper($this->getDayNameInCroatian($date->dayOfWeek)),
+                $this->totalWorkHours ?: '',
+                $this->totalVacationHours ?: '',
+                $this->totalSickLeaveHours ?: '',
+                $this->totalOvertimeHours ?: '',
+                $this->totalOtherHours ?: '',
             ];
         }
-        
-        // Dodaj prazne redove
+
+        // Add empty rows
         $data[] = ['', '', '', '', '', '', ''];
         $data[] = ['', '', '', '', '', '', ''];
         $data[] = ['', '', '', '', '', '', ''];
-        
-        // Dodaj ukupno
+
+        // Add totals row
         $monthName = $this->getMonthNameInCroatian($this->month);
-        $data[] = ["UKUPNO $monthName $this->year.", '', '', '', '', '', ''];
-        $data[] = ['RADNI SATI', '', $this->formatMinutesToHours($totalWorkHours), '', '', '', ''];
-        $data[] = ['GODIŠNJI ODMOR', '', '', $this->formatMinutesToHours($totalVacationHours), '', '', ''];
-        $data[] = ['BOLOVANJE', '', '', '', $this->formatMinutesToHours($totalSickLeaveHours), '', ''];
-        $data[] = ['PREKOVREMENI SATI', '', '', '', '', $this->formatMinutesToHours($totalOvertimeHours), ''];
-        $data[] = ['OSTALO', '', '', '', '', '', $this->formatMinutesToHours($totalOtherHours)];
+        $data[] = [
+            "UKUPNO {$monthName} {$this->year}.",
+            '',
+            '',
+            '',
+            '',
+            '',
+            ''
+        ];
+        $data[] = [
+            'RADNI SATI',
+            $this->totalWorkHours,
+            '',
+            '',
+            '',
+            '',
+            ''
+        ];
+        $data[] = [
+            'GODIŠNJI ODMOR',
+            $this->totalVacationHours,
+            '',
+            '',
+            '',
+            '',
+            ''
+        ];
+        $data[] = [
+            'BOLOVANJE',
+            $this->totalSickLeaveHours,
+            '',
+            '',
+            '',
+            '',
+            ''
+        ];
+        $data[] = [
+            'PREKOVREMENI SATI',
+            $this->totalOvertimeHours,
+            '',
+            '',
+            '',
+            '',
+            ''
+        ];
+        $data[] = [
+            'OSTALO',
+            $this->totalOtherHours,
+            '',
+            '',
+            '',
+            '',
+            ''
+        ];
 
         return $data;
     }
@@ -92,7 +127,7 @@ class EmployeeReportExport implements FromArray, WithHeadings, WithStyles, Shoul
     {
         return [
             'DATUM',
-            'DAN', 
+            'DAN',
             'RADNI SATI',
             'GODIŠNJI ODMOR',
             'BOLOVANJE',
@@ -101,75 +136,89 @@ class EmployeeReportExport implements FromArray, WithHeadings, WithStyles, Shoul
         ];
     }
 
-    public function styles(Worksheet $sheet)
+    public function styles(\PhpOffice\PhpSpreadsheet\Worksheet\Worksheet $sheet): array
     {
-        $styles = [
-            1 => ['font' => ['bold' => true]], // Header row
+        return [
+            // center header row and first column
+            1 => [
+                'font' => ['bold' => true],
+                'alignment' => [
+                    'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+                ],
+            ],
+            "C1:C99" => [
+                'alignment' => [
+                    'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+                ],
+            ],
+            "D1:D99" => [
+                'alignment' => [
+                    'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+                ],
+            ],
+            "E1:E99" => [
+                'alignment' => [
+                    'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+                ],
+            ],
+            "F1:F99" => [
+                'alignment' => [
+                    'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+                ],
+            ],
+            "G1:G99" => [
+                'alignment' => [
+                    'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+                ],
+            ],
+            'A2:A32' => [
+                'alignment' => [
+                    'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+                ],
+            ],
+            'A32:A40' => [
+                'font' => ['bold' => true],
+            ],
         ];
-        
-        // Style weekend rows (Saturday = 6, Sunday = 0)
-        $daysInMonth = Carbon::create($this->year, $this->month)->daysInMonth;
-        
-        for ($day = 1; $day <= $daysInMonth; $day++) {
-            $date = Carbon::create($this->year, $this->month, $day);
-            if ($date->isWeekend()) {
-                $rowNumber = $day + 1; // +1 because row 1 is header
-                $styles[$rowNumber] = [
-                    'fill' => [
-                        'fillType' => Fill::FILL_SOLID,
-                        'color' => ['rgb' => 'F3F4F6'], // Light gray
-                    ]
-                ];
-            }
-        }
-        
-        return $styles;
     }
 
-    private function getDailyWorkHours(Carbon $date): string
+    private function getDailyWorkHours(Carbon $date): float
     {
         $timeLogs = TimeLog::where('employee_id', $this->employeeId)
             ->whereDate('date', $date->format('Y-m-d'))
             ->get();
-        
         $totalMinutes = 0;
         foreach ($timeLogs as $timeLog) {
-            $totalMinutes += TimeLog::convertTimeToMinutes($timeLog->hours);
+            $totalMinutes += (float)$timeLog->hours;
         }
-        
-        return $totalMinutes > 0 ? TimeLog::formatMinutesToTime($totalMinutes) : '';
+
+        return $totalMinutes;
     }
 
-    private function getLeaveHours(Carbon $date, string $type): string
+    private function getLeaveHours(Carbon $date, LeaveRequestType $type): ?int
     {
-        $typeMapping = [
-            'vacation' => 'vacation',
-            'sick_leave' => 'sick_leave', 
-            'other' => 'paid_leave'
-        ];
-        
         $leaveRequest = LeaveRequest::where('employee_id', $this->employeeId)
-            ->where('type', $typeMapping[$type])
+            ->where('type', $type->value)
             ->where('status', 'approved')
             ->whereDate('start_date', '<=', $date->format('Y-m-d'))
             ->whereDate('end_date', '>=', $date->format('Y-m-d'))
             ->first();
-            
-        return $leaveRequest ? '8' : '';
+
+        return $leaveRequest ? 8 : null;
     }
 
     private function getDayNameInCroatian(int $dayOfWeek): string
     {
         $days = [
             0 => 'Nedjelja',
-            1 => 'Ponedjeljak', 
+            1 => 'Ponedjeljak',
             2 => 'Utorak',
             3 => 'Srijeda',
             4 => 'Četvrtak',
             5 => 'Petak',
             6 => 'Subota'
         ];
-        
+
         return $days[$dayOfWeek];
     }
 
@@ -177,7 +226,7 @@ class EmployeeReportExport implements FromArray, WithHeadings, WithStyles, Shoul
     {
         $months = [
             1 => 'SIJEČANJ',
-            2 => 'VELJAČA', 
+            2 => 'VELJAČA',
             3 => 'OŽUJAK',
             4 => 'TRAVANJ',
             5 => 'SVIBANJ',
@@ -189,17 +238,7 @@ class EmployeeReportExport implements FromArray, WithHeadings, WithStyles, Shoul
             11 => 'STUDENI',
             12 => 'PROSINAC'
         ];
-        
+
         return $months[$month];
-    }
-
-    private function convertTimeToMinutes(string $time): int
-    {
-        return TimeLog::convertTimeToMinutes($time);
-    }
-
-    private function formatMinutesToHours(int $minutes): string
-    {
-        return $minutes > 0 ? TimeLog::formatMinutesToTime($minutes) : '';
     }
 }
