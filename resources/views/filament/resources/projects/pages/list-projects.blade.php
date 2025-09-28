f47fd161fb227df48aa1751e850c20db3f5b6fd5
Tomislav Lazar <tomislav@morgancode.com>
2025-09-28 15:59:20 +0200

    update

--- resources/views/filament/resources/projects/pages/list-projects.blade.php
+++ resources/views/filament/resources/projects/pages/list-projects.blade.php
@@ -5,17 +5,13 @@
 <x-filament-panels::page>
     <div class="space-y-6" wire:ignore>
 
-        @if(isset($statusProjekta))
-            @livewire('filament-employee-management::projects.project-table', ['status' => $statusProjekta], key('project-table-'.$statusProjekta->value))
-        @else
-            @foreach([
-                StatusProjekta::Priprema,
-                StatusProjekta::Provedba,
-                StatusProjekta::Finalizacija
-            ] as $status)
-                @livewire('filament-employee-management::projects.project-table', ['status' => $status], key('project-table-'.$status->value))
-            @endforeach
-        @endif
+        @foreach([
+            StatusProjekta::Priprema,
+            StatusProjekta::Provedba,
+            StatusProjekta::Finalizacija
+        ] as $status)
+            @livewire('filament-employee-management::projects.project-table', ['status' => $status], key('project-table-'.$status->value))
+        @endforeach
     </div>
 
     <x-filament-actions::modals/>
---
+++ resources/views/filament/resources/projects/pages/project-by-status.blade.php
@@ -0,0 +1,13 @@
+@php
+    use Amicus\FilamentEmployeeManagement\Enums\StatusProjekta;
+@endphp
+
+<x-filament-panels::page>
+    <div class="space-y-6" wire:ignore>
+
+
+
+    </div>
+
+    <x-filament-actions::modals/>
+</x-filament-panels::page>
